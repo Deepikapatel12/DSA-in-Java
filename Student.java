1e8bf35673f22a6b14efe448173043c517697e92
Deepikapatel <deepikapatel5687@gmail.com>
2024-08-01 22:07:17 +0530

    Singly Linked List

---
+++ Student.java
@@ -0,0 +1,17 @@
+class Student{
+	public static void main(String args[]){
+	
+	int rollno=101;
+	int mobile=2588798;
+	String name="Deepu";
+	String email="Deepika05@gmail.com";
+
+	System.out.println("Name :"+name);
+	System.out.println("Rollno :"+rollno);
+	System.out.println("Mobile numm :"+mobile);
+	System.out.println("Email :"+email);
+
+
+	}
+	}
+	 
