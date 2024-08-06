844b38cf731d7e1757a117fffd85612af224457d
Deepikapatel <deepikapatel5687@gmail.com>
2024-08-06 22:42:14 +0530

    Remdom

--- Remdom-Generator-Demo/src/MyMain.java
+++ Remdom-Generator-Demo/src/MyMain.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class MyMain {
+import java.util.Random;
+
+public class MyMain {
+    public static void main(String[] args) {
+        Random random = new Random();
+//        System.out.println("random.nextInt() = " + random.nextInt());
+//        Generate 1000 random numbers
+        int upperBound = 1000;
+        for (int i = 0; i < 1000; i++) {
+//            System.out.println(random.nextInt(1000));
+            System.out.println(random.nextInt(upperBound));
+        }
+    }
 }
